a012311d99e16c6fbaa81339f9f0a5cb7ba6d3a3
申公 <shengong.yz@alibaba-inc.com>
2025-05-01 17:40:56 +0800

    bmi计算

--- io-practice/InputPractice.py
+++ io-practice/InputPractice.py
@@ -1,9 +1,19 @@
 import datetime
 
-var = input(
-    '''********************
-*******input********
-********************''')
-print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-print(var)
-print("finished")
+height = float(input("请输入你的身高(单位m)："))
+weight = float(input("请输入你的体重(单位千克)："))
+
+bmi = weight / (height * height)
+
+if bmi < 18.5:
+    result = '体重过轻'
+elif bmi < 24.9:
+    result = '体重正常'
+elif bmi < 29.9:
+    result = '体重过重'
+else:
+    result = '肥胖'
+print("您的bmi是: ", bmi)
+print(result)
+
+
