fd83ed36de8da64995cf86061161ef5872ca8843
申公 <shengong.yz@alibaba-inc.com>
2025-05-01 17:20:15 +0800

    feat: create InputPractice.py

---
+++ io-practice/InputPractice.py
@@ -0,0 +1,8 @@
+import datetime
+
+var = input(
+    '''********************
+*******input********
+********************''')
+print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+print(var)
